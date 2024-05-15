fb61f226a93f6cff1c27861ee65653a15c7f2b10
gdimascio <guido.dimascio@gmail.com>
2024-05-15 20:13:07 -0300

    nodemailer

---
+++ index.js
@@ -0,0 +1,28 @@
+
+require("dotenv").config();
+
+const express = require("express");
+const router = require("./router/router")
+const hbs = require("hbs");
+
+const app = express();
+
+// HBS
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+// MIDDLEWARE
+app.use(express.static(__dirname + "/public"));
+
+// Configurar express para procesar datos de formulario
+app.use(express.urlencoded({extended: false}));
+
+
+app.use("/", router)
+
+
+const port = process.env.PORT || 3001;
+app.listen(port , () => {
+    console.log("usando el puerto http://localhost:" + port);
+})
